8e9a502d7f2ca92342732a7a8260ba6cd98219ad
vedant-vedant <116245798+vedant-vedant@users.noreply.github.com>
2023-12-29 07:18:45 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,17 @@
+"use strict"; // means we are newer version of JS
+
+// alert(3+3) we are using node js not browser, so this will not work
+/*
+Data types
+1 number
+2 bigInt
+string = ""
+boolean = true/false
+null= standalone value, is a object  
+symbol = it is use to define uniqueness mostly use in react
+undefined is a type of variable
+
+object
+*/
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
