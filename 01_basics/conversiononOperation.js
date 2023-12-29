990fac0d2458525955090eea4c45be5f14a6229e
vedant-vedant <116245798+vedant-vedant@users.noreply.github.com>
2023-12-29 07:41:13 +0000

    Conversion of data type

---
+++ 01_basics/conversiononOperation.js
@@ -0,0 +1,22 @@
+// let score = "33abc"
+// console.log(typeof score)
+
+// let valueinnumber = Number(score) //typecasting of score
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);
+
+/*
+conversion result of different value
+"33"= 33
+"33abc" = NaN
+true = 1; false = 0;
+Null = 0
+*/
+
+let isLoggedIn = "hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn) //typecasting
+console.log(booleanIsLoggedIn); 
+
+// various outputs
+// 1 => true; 0= false; 
+// string = true mein convert and empty string ko false
